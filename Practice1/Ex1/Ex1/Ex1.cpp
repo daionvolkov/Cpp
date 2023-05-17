49ce109f85f836341be2f234dc691aa733a01112
daionvolkov <daionvolkov@gmail.com>
2023-05-17 11:22:07 +0300

    commit prattice1 17.05

---
+++ Practice1/Ex1/Ex1/Ex1.cpp
@@ -0,0 +1,14 @@
+﻿// Ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What is your name? ";
+    getline(cin, name);
+    cout << "Hello, " << name << "!\n";
+}
---
+++ Practice1/Ex3/Ex3/Ex3.cpp
@@ -0,0 +1,30 @@
+﻿// Ex3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main()
+{
+    double perimeter;
+    double halfPerimeter;
+    double side; 
+    double s;
+
+
+    std::cout << "Enter the perimeter of triangle: ";
+    std::cin >> perimeter;
+    side = perimeter / 3.0;
+    halfPerimeter = perimeter / 2;
+
+    s = std::sqrt(halfPerimeter *(halfPerimeter -side) * (halfPerimeter - side) * (halfPerimeter - side));
+
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Side\tArea\n";
+    std::cout << side << "\t" << s << std::endl;
+
+    return 0;
+
+}
+    
